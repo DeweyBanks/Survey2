741c3ebef1676e88f84b40bb8c3c1f966a7468b3
Dewey Banks <DeweyBanks@gmail.com>
2017-04-11 12:44:35 -0400

    locally working sendgrid

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -3,21 +3,12 @@ include SendGrid
 require 'json'
 
 class FeedbackMailer < ApplicationMailer
-
+   default :from => 'admin@surveymaker.com'
 
   def feedback_email(feedback)
     @feedback = feedback
-    from = Email.new(email: 'admin@surveymaker.com')
-    subject = "You have feedback from #{@feedback.email}"
-    to = Email.new(email: ENV['ADMIN_EMAIL'])
-    content = Content.new(type: 'text/plain', value: @feedback.body)
-    mail = Mail.new(from, subject, to, content)
-    # puts JSON.pretty_generate(mail.to_json)
-    puts mail.to_json
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
+    @url = 'https://survey-constructor.herokuapp.com/admin'
+     mail( :to => ENV['ADMIN_EMAIL'],
+    :subject => 'Feedback from SurveyMaker' )
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,15 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'https://survey-constructor.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
