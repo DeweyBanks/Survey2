81745bfa62dddae603be80ecc2dd9cfc75795b22
Dewey Banks <DeweyBanks@gmail.com>
2017-06-10 16:23:37 -0400

    adds geo track to index page'

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -1,3 +1,11 @@
+var trackId = null;
+var locations = [];
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 30000
+}
+
 function displayLocation(position) {
 
   var latitude = position.coords.latitude;
@@ -15,7 +23,59 @@ function displayError(error) {
 }
 
 if(navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(displayLocation, displayError)
+  navigator.geolocation.getCurrentPosition(displayLocation, displayError, options)
 } else {
   alert("Sorry, this browser does not support geolocation");
 }
+
+
+function trackMe() {
+  trackId = navigator.geolocation.watchPosition(displayLocation, displayError);
+}
+
+function clearTracking() {
+  if (trackId) {
+    navigator.geolocation.clearWatch(trackId);
+    trackId = null;
+  }
+}
+
+function computeTotalDistance() {
+  var totalDistance = 0;
+
+  if (locations.length > 1) {
+    for (var i = i; i < locations.length; i++) {
+      totalDistance += google.maps.geometry.spherical.computeDistanceBetween(locations[i-1], locations[i]);
+    }
+  }
+  return totalDistance;
+}
+
+var pDistance = document.getElementById("distance");
+var trackButton = document.querySelector("button");
+
+trackButton.onclick = function(e) {
+  e.preventDefault();
+
+  if (trackButton.innerHTML === "Start") {
+    trackButton.innerHTML = "Stop";
+    trackMe();
+  } else {
+    trackButton.innerHTML = "Start";
+    clearTracking();
+    var d = computeTotalDistance();
+    var miles = d/1.6;
+    if (d > 0) {
+      d = Math.round(d * 1000) / 1000;
+      pDistance.innerHTML = "Total distance traveled: " + miles + "miles";
+    } else {
+      pDistance.innerHTML = "You didn't travel anywhere at all.";
+    }
+  }
+};
+
+
+
+
+
+
