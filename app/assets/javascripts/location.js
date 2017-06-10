8ba93b44e44ef47a379464df1855254548d486f6
Dewey Banks <DeweyBanks@gmail.com>
2017-06-10 14:34:54 -0400

    removes map

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -1,42 +1,21 @@
+function displayLocation(position) {
 
-  // console.log("called");
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
 
+  var pLocation = document.getElementById("location");
+  console.log("Loc:: ", pLocation);
+  pLocation.innerHTML += latitude + ", " + longitude + "<br>";
+}
 
-  // linkElectionInfo();
+function displayError(error) {
+  var errors = ["Unknown error", "Permission Denied by user", "Position not availaable", "Timeout error" ];
+  var message = errors[error.code];
+  console.warn("Error in getting your location: " + message, error.message);
+}
 
-  // function linkElectionInfo() {
-  //   console.log("called2");
-  //   var links = document.getElementsByClassName("election-info-btn");
-
-  //   Array.prototype.forEach.call(links, function(link) {
-  //     link.classList.add('loading');
-  //   });
-
-  //   console.log("links", links);
-  //   navigator.geolocation.getCurrentPosition(function(position) {
-  //     console.log("called3");
-  //     var lat = position.coords.latitude,
-  //         lng = position.coords.longitude,
-  //         // url = "//sheltered-escarpment-31930.herokuapp.com/local_election_links?lat=" + lat + "&lng=" + lng;
-  //         url = "//localhost:3000/local_election_links?lat=" + lat + "&lng=" + lng;
-  //         console.log("url::: ", url);
-
-  //     $.getJSON(url, function(data) {
-  //       var senateDiv = document.getElementById("senate-link"),
-  //           houseDiv = document.getElementById("house-link"),
-  //           senateLink = senateDiv.querySelector('.election-info-btn'),
-  //           houseLink = houseDiv.querySelector('.election-info-btn'),
-  //           senateLinkText = senateLink.querySelector('.text'),
-  //           houseLinkText = houseLink.querySelector('.text');
-
-  //       senateLink.href = data.senate_url;
-  //       houseLink.href = data.house_url;
-  //       senateLinkText.innerText = data.senate_text;
-  //       houseLinkText.innerText = data.house_text;
-
-  //       senateLink.classList.remove('loading');
-  //       houseLink.classList.remove('loading');
-  //     });
-  //   });
-  // }
-
+if(navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(displayLocation, displayError)
+} else {
+  alert("Sorry, this browser does not support geolocation");
+}
